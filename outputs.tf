93e8e5fabede104dff0c4774bada8b5580fe4bd2
Vlad Gorokhov <vlad.gorokh@gmail.com>
2025-03-05 03:04:39 +0000

    more outputs added

--- outputs.tf
+++ outputs.tf
@@ -7,3 +7,19 @@ output "kubeconfig" {
   value     = talos_cluster_kubeconfig.this.kubeconfig_raw
   sensitive = true
 }
+
+output "private_network_id" {
+  value = hcloud_network.network.id
+}
+
+output "control_plane_ip" {
+  value = hcloud_server.control_plane.ipv4_address
+}
+
+output "nat_server_id" {
+  value = hcloud_server.nat.id
+}
+
+output "nat_server_ip" {
+  value = hcloud_primary_ip.natipv4.ip_address
+}
